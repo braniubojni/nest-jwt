54710f472e68c6933b520d086aef46a1c301cbbc
Erik <braniubojni@mail.ru>
2022-05-01 13:52:03 +0400

    eh/User-is-registered

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { SequelizeModule } from "@nestjs/sequelize";
+import { User } from "./users/users.model";
 import { UsersModule } from './users/users.module';
 
 @Module({
@@ -10,12 +11,12 @@ import { UsersModule } from './users/users.module';
     }),
     SequelizeModule.forRoot({
       dialect: "postgres",
-      host: process.env.POSTGRES_HOST,
-      port: +process.env.POSTGRES_PORT,
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: +process.env.POSTGRES_PORT || 5432,
       username: process.env.POSTGRES_USER || 'postgres',
       password: process.env.POSTGRES_PASSWORD || 'xiaomi',
       database: process.env.POSTGRES_DB,
-      models: [],
+      models: [User],
       autoLoadModels: true,
     }),
     UsersModule,
